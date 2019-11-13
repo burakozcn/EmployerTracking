acd1861474b1c6cdce6f33a6ee34a1130aa892dd
Burak Ozcan <ozcan.burak@gmail.com>
2019-11-13 11:08:28 +0300

    Layout & Unit Tests

---
+++ EmployerTrackingTests/CategoryListViewModelTest.swift
@@ -0,0 +1,12 @@
+import XCTest
+
+class CategoryListViewModelTest: XCTestCase {
+  
+  override func setUp() {
+    continueAfterFailure = false
+    XCUIApplication().launch()
+  }
+  
+  override func tearDown() {
+  }
+}
